e29bac25b195fcd6bbf445c5e8d0c154b8b31c97
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-09-12 13:26:08 +0100

    Por o tailwind a funcionar

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -1,19 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
 export default config;
