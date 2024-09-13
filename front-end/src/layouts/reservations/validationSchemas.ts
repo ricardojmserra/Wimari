63446d390b63dede81e4ad8993222186cb5492e7
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-09-13 14:43:19 +0100

    Changing validation messages

--- front-end/src/layouts/reservations/validationSchemas.ts
+++ front-end/src/layouts/reservations/validationSchemas.ts
@@ -1,19 +1,20 @@
 import * as Yup from 'yup';
 
 export const personalInfoValidationSchema = Yup.object({
-	name: Yup.string().required('First name is required'),
-	lastName: Yup.string().required('Last name is required'),
+	name: Yup.string().required('You must insert your first name'),
+	lastName: Yup.string().required('You must insert your last name'),
 	phone: Yup.string()
 		.matches(/^(\+\d{1,3})?\d{9}$/, 'Phone number is not valid')
-		.required('Phone number is required'),
+		.required('You must insert your phone number'),
 	obs: Yup.string(),
 });
 
 export const reservationInfoValidationSchema = Yup.object({
-	date: Yup.date().required('Date is required'),
+	date: Yup.date().required('You must select a date'),
 	time: Yup.string(),
-	// .required('Time is required'),
+	// .required('You must select a reservation time'),
 	persons: Yup.number()
-		.required('Number of persons is required')
-		.min(1, 'Must be at least 1 person'),
+		.typeError('You must select a number of persons')
+		.required('You must select a number of persons')
+		.min(1, 'You must select at least 1 person'),
 });
