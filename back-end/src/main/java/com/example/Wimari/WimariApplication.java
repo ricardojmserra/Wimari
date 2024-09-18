c44f008fd11bce0e4bfdcf9cff5c6479853e8821
Will4you <a68016@ualg.pt>
2024-09-19 00:47:10 +0100

    first commit

--- back-end/src/main/java/com/example/Wimari/WimariApplication.java
+++ back-end/src/main/java/com/example/Wimari/WimariApplication.java
@@ -1,21 +1,28 @@
-package com.example.demo;
+package com.example.Wimari;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 @RestController
-public class WimariApplication {
+// public class WimariApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WimariApplication.class, args);
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(WimariApplication.class, args);
+// 	}
+// 	@GetMapping("/hello")
+//     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//       return String.format("Hello %s!", name);
+//     }
+
+// }
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public  class  Main{
+	public static void main (String[], args){
+		SpringApplication.run(Main.class, args);
+
 	}
-	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
-
 }
